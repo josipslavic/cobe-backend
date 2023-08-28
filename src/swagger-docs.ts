438436eadf90eabefe38658d33e2478c0c9984de
Josip Slavic <jopisla@gmail.com>
2023-08-28 14:23:59 +0200

    Added Swagger documentation

---
+++ src/swagger-docs.ts
@@ -0,0 +1,8 @@
+import swaggerUi from 'swagger-ui-express';
+import { Express } from 'express';
+import swaggerDocument from './swagger.json';
+
+export function swaggerDocs(app: Express, port: number) {
+  app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+  console.info(`Docs available at http://localhost:${port}/api-docs`);
+}
