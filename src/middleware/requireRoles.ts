a66794f10798e4e2713af0fbd3624e3a8df9914f
Josip Slavic <jopisla@gmail.com>
2023-09-04 11:02:03 +0200

    Importing messages as a whole for readabilty

--- src/middleware/requireRoles.ts
+++ src/middleware/requireRoles.ts
@@ -1,11 +1,13 @@
 import { Response, NextFunction } from 'express';
 import { RequestWithUserId } from './isAuth';
-import { PERMISSION_DENIED } from '../constants/error-messages';
+import * as ERROR_MESSAGES from '../constants/error-messages';
 
 export function requireRoles(roles: ('admin' | 'editor' | 'guest')[]) {
   return async (req: RequestWithUserId, res: Response, next: NextFunction) => {
     if (!roles.includes(req.user!.role))
-      return res.status(401).json({ errors: [PERMISSION_DENIED] });
+      return res
+        .status(401)
+        .json({ errors: [ERROR_MESSAGES.PERMISSION_DENIED] });
     next();
   };
 }
