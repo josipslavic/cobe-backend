e37eb04a9e8e8ed75f2f6f69d09ee8b3025214bb
Josip Slavic <jopisla@gmail.com>
2023-09-04 10:57:31 +0200

    Edited messages

--- src/constants/error-messages.ts
+++ src/constants/error-messages.ts
@@ -1,15 +1,15 @@
-export const NO_VALUES_PROVIDED = 'you have to provide at least one value';
+export const NO_VALUES_PROVIDED = 'You have to provide at least one value';
 
-export const PERMISSION_DENIED = "you don't have permission to do this action";
+export const PERMISSION_DENIED = "You don't have permission to do this action";
 
-export const AUTH_FAILED = 'auth failed';
+export const AUTH_FAILED = 'Auth failed';
 
-export const INVALID_CREDENTIALS = 'invalid email or password';
+export const INVALID_CREDENTIALS = 'Invalid email or password';
 
-export const INVALID_MONGO_ID = 'please enter a valid mongo id';
+export const INVALID_MONGO_ID = 'Please enter a valid mongo id';
 
-export const NEWS_NOT_FOUND = 'news not found';
+export const NEWS_NOT_FOUND = 'News not found';
 
-export const COMMENT_NOT_FOUND = 'comment not found';
+export const COMMENT_NOT_FOUND = 'Comment not found';
 
-export const BREAKING_NEWS_LIMIT = 'only one breaking news is alowed';
+export const BREAKING_NEWS_LIMIT = 'Only one breaking news is alowed';
