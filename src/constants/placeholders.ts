c5d6cb8752f43ebc50c5efcc95003d7629480b03
Josip Slavic <jopisla@gmail.com>
2023-09-04 10:57:31 +0200

    Edited messages

--- src/constants/placeholders.ts
+++ src/constants/placeholders.ts
@@ -1,6 +1,6 @@
 export const newsPlaceholders = {
   description: ' ',
   urlToImage:
-    'https://upload.wikimedia.org/wikipedia/commons/thumb/3/3f/Placeholder_view_vector.svg/681px-Placeholder_view_vector.svg.png', // TODO: We don't want to have external url-s for pictures.
+    'https://upload.wikimedia.org/wikipedia/commons/thumb/3/3f/Placeholder_view_vector.svg/681px-Placeholder_view_vector.svg.png', // TODO: We don't want to have external url-s for pictures, will be fixed in different PR
   author: 'Unkown author',
 };
